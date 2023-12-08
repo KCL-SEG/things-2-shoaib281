2feefa7642a3b934ad042c5e50eb7c1ff34f15df
MOHAMMAD (SHOAIB) SHOAIB <mohammad.shoaib@kcl.ac.uk>
2023-12-08 15:39:23 +0000

    another one

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,20 @@
 """Forms of the project."""
 
 # Create your forms here.
+from django import forms
+from .models import Thing
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+
+    description = forms.CharField(widget=forms.Textarea)
+    quantity = forms.IntegerField(widget=forms.NumberInput)
+
+    def clean_quantity(self):
+        quantity = self.cleaned_data.get('quantity')
+        if quantity is not None and quantity < 0:
+            raise forms.ValidationError("invalid")
+        return quantity
+
